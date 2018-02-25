25893b046a59d48aada9f7fbbc03846bc1eea6e2
Joseph McCormick <esmevane@gmail.com>
2018-02-25 11:21:55 -0600

    Daemonize and Rocket.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate daemonize;
+extern crate rocket;
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate serde_derive;
+
+use daemonize::Daemonize;
+use rocket_contrib::Json;
+
+#[derive(Serialize, Deserialize)]
+enum HealthStatus {
+    Healthy,
+    Unhealthy,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Health {
+    status: HealthStatus,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Index {}
+
+#[get("/")]
+fn index() -> Option<Json<Index>> {
+    Some(Json(Index {}))
+}
+
+#[get("/health")]
+fn health() -> Option<Json<Health>> {
+    Some(Json(Health {
+        status: HealthStatus::Healthy,
+    }))
+}
+
 fn main() {
-    println!("Hello, world!");
+    Daemonize::new()
+        .working_directory("/tmp")
+        .pid_file("/tmp/notectl.pid")
+        .start()
+        .unwrap();
+
+    rocket::ignite().mount("/", routes![index, health]).launch();
 }
