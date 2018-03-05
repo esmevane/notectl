4ee2fe1f84ba11283f7b74ef950a58a1ee2a768e
Joseph McCormick <esmevane@gmail.com>
2018-03-04 21:59:47 -0600

    @wip

--- src/main.rs
+++ src/main.rs
@@ -14,42 +14,107 @@ use service::Service;
 mod api;
 mod service;
 
-fn start() {
-    let service = Service::empty();
-
-    service.start(|| Api::new().run());
+trait Command {
+    fn about(&self) -> &'static str;
+    fn execute(&self);
+    fn init() -> Self;
+    fn name(&self) -> &'static str;
 }
 
-fn stop() {
-    let service = Service::empty();
+struct Pid {}
 
-    service.stop();
+impl Command for Pid {
+    fn init() -> Self
+    where
+        Self: Command,
+    {
+        Pid {}
+    }
+
+    fn about(&self) -> &'static str {
+        "Displays the PID"
+    }
+
+    fn execute(&self) {
+        let service = Service::empty();
+        match service.id() {
+            Some(pid) => println!("{}", pid),
+            None => println!("Unable to locate running notectl process"),
+        }
+    }
+
+    fn name(&self) -> &'static str {
+        "pid"
+    }
 }
 
-fn print_pid() {
-    let service = Service::empty();
-    match service.id() {
-        Some(pid) => println!("{}", pid),
-        None => println!("Unable to locate running notectl process"),
+struct Start {}
+
+impl Command for Start {
+    fn init() -> Self
+    where
+        Self: Command,
+    {
+        Start {}
+    }
+
+    fn about(&self) -> &'static str {
+        "Starts the service"
+    }
+
+    fn execute(&self) {
+        let service = Service::empty();
+
+        service.start(|| Api::new().run());
+    }
+
+    fn name(&self) -> &'static str {
+        "start"
+    }
+}
+
+struct Stop {}
+
+impl Command for Stop {
+    fn init() -> Self
+    where
+        Self: Command,
+    {
+        Stop {}
+    }
+
+    fn about(&self) -> &'static str {
+        "Stops the service"
+    }
+
+    fn execute(&self) {
+        let service = Service::empty();
+        service.stop();
+    }
+
+    fn name(&self) -> &'static str {
+        "stop"
     }
 }
 
 fn main() {
+    let start = Start::init();
+    let stop = Stop::init();
+    let pid = Pid::init();
+
     let matches = App::new("notectl")
         .version("0.0.1")
         .about("A note handling core service")
         .author("Joseph McCormick <esmevane@gmail.com>")
-        .subcommand(SubCommand::with_name("start").about("Starts the service"))
-        .subcommand(SubCommand::with_name("stop").about("Stops the service"))
-        .subcommand(
-            SubCommand::with_name("pid").about("Returns the process id"),
-        )
+        .subcommand(SubCommand::with_name(start.name()).about(start.about()))
+        .subcommand(SubCommand::with_name(stop.name()).about(stop.about()))
+        .subcommand(SubCommand::with_name(pid.name()).about(pid.about()))
         .get_matches();
 
-    match matches.subcommand() {
-        ("start", Some(_)) => start(),
-        ("stop", Some(_)) => stop(),
-        ("pid", Some(_)) => print_pid(),
+    match matches.subcommand_name() {
+        Some(command) if command == start.name() => start.execute(),
+        Some(command) if command == stop.name() => stop.execute(),
+        Some(command) if command == pid.name() => pid.execute(),
         _ => (),
     };
 }
